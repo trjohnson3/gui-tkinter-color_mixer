3d509bd40d3b4294b41ca59663e9eb3fe0c9391d
Tom Johnson <trjohnson3@gmail.com>
2024-06-19 19:50:03 -0500

    initial setup

---
+++ color_mixer.py
@@ -0,0 +1,21 @@
+import tkinter
+
+#Define window
+root = tkinter.Tk()
+root.title('Color Mixer')
+root.iconbitmap('')
+root.geometry('400x600')
+root.resizable(0,0)
+
+#Define fonts and colors
+
+
+#Define functions
+
+
+#GUI Layout
+
+
+#Run app
+root.mainloop()
+
